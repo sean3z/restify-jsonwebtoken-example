ce64fc14a0de9fda3f2734bd7c2abaea3d8fb376
Sean Wragg <sean@lappy486.cfl.rr.com>
2018-05-29 22:13:31 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,34 @@
+"use strict";
+
+const restify = require('restify');
+const rjwt = require('restify-jwt-community');
+const jwt = require('jsonwebtoken');
+const config = require('./config');
+const user = require('./lib/user');
+const app = restify.createServer();
+
+app.use(restify.plugins.queryParser());  
+app.use(restify.plugins.bodyParser());
+app.use(rjwt(config.jwt).unless({
+    path: ['/ping', '/auth']
+}));
+
+app.get('/user', (req, res, next) => {
+    res.send(req.user);
+});
+
+app.post('/auth', (req, res, next) => {
+    let { username, password } = req.body;
+    user.authenticate(username, password).then(data => {
+        let token = jwt.sign(data, config.jwt.secret, {
+            expiresIn: '1m'
+        });
+
+        let {iat, exp} = jwt.decode(token);
+        res.send({iat, exp, token});
+    })
+});
+
+app.listen(8080, () => {  
+    console.log('%s listening at %s', app.name, app.url);
+});
